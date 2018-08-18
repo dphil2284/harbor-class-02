9b240122eaca127ea56bd1e5a0e194d6dafc764b
dphil2284 <d_phil@naver.com>
2018-08-18 18:23:16 +0900

    save master

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,6 @@
 import React from "react";
-import Page152 from "./Page152";
 import Page1298 from "./Page1298";
-import Page55 from "./Page55";
 import HomePage from "./HomePage";
-import Page6 from "./Page6";
-import Page913 from "./Page913";
-import Page106 from "./Page106";
 
 class App extends React.Component {
   render() {
@@ -20,22 +15,12 @@ class App extends React.Component {
 export default {
   path: "/",
   component: App,
-  indexRoute: { component: Page152 },
+  indexRoute: { component: Page1298 },
   childRoutes: [
-    {
-      path: "/class-03",
-      name: "/class-03",
-      component: Page152
-    },
     {
       path: "/Portfolio-01",
       name: "/Portfolio-01",
       component: Page1298
-    },
-    {
-      path: "/class-04",
-      name: "/class-04",
-      component: Page55
     },
     {
       path: "/home",
@@ -43,24 +28,9 @@ export default {
       component: HomePage
     },
     {
-      path: "/work",
-      name: "/work",
-      component: Page6
-    },
-    {
-      path: "/harbor-class2",
-      name: "/harbor-class2",
-      component: Page913
-    },
-    {
-      path: "/col",
-      name: "/col",
-      component: Page106
-    },
-    {
       path: "*",
       name: "notfound",
-      component: Page152
+      component: Page1298
     }
   ]
 };
